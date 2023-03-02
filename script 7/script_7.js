359462f4f67264c5914ce1b094cbb28ac70fa0fe
sbellotlecoq <sbellotlecoq@gmail.com>
2023-03-02 20:33:01 +0100

    7 YEAH fini

--- script 7/script_7.js
+++ script 7/script_7.js
@@ -4,10 +4,12 @@ if (answer.includes("?")) {
     console.log("ouai ouai")
   } else if (answer.match(/[A-Z]+/)) {
     console.log("Pwa, calme-toi...");
-  } else if (answer.includes("Fortnite?")) {
+  } else if (answer.includes("Fortnite")) {
     console.log("on s'fait une partie soum-soum ?")
-  } else if (answer.isEmpty({})) {
+  } else if (answer === ("")) {
     console.log("t'es en PLS ?")
+  } else {
+    console.log("Balek")
   } 
 
 
