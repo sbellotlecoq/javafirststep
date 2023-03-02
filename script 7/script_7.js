07111923032c4df2fbce2fa63929addfdbe1ca2f
sbellotlecoq <sbellotlecoq@gmail.com>
2023-03-02 20:20:13 +0100

    7 mais pas fini

--- script 7/script_7.js
+++ script 7/script_7.js
@@ -1,39 +1,15 @@
-function translateARNtoProtein(ARN) {
-  // Définition du tableau des codons et des acides aminés correspondants
-  const codons = {
-    UCU: 'Sérine', UCC: 'Sérine', UCA: 'Sérine', UCG: 'Sérine',
-    AGU: 'Sérine', AGC: 'Sérine', CCC: 'Proline', CCU: 'Proline',
-    CCA: 'Proline', CCG: 'Proline', UUA: 'Leucine', UUG: 'Leucine',
-    UUU: 'Phénylalanine', UUC: 'Phénylalanine', CGU: 'Arginine',
-    CGC: 'Arginine', CGA: 'Arginine', CGG: 'Arginine',
-    AGA: 'Arginine', AGG: 'Arginine', UAU: 'Tyrosine',
-    UAC: 'Tyrosine'
-  };
+let answer = "";
+answer = prompt("question ?")
+if (answer.includes("?")) {
+    console.log("ouai ouai")
+  } else if (answer.match(/[A-Z]+/)) {
+    console.log("Pwa, calme-toi...");
+  } else if (answer.includes("Fortnite?")) {
+    console.log("on s'fait une partie soum-soum ?")
+  } else if (answer.isEmpty({})) {
+    console.log("t'es en PLS ?")
+  } 
+
+
+
   
-  let protein = '';
-  
-  // Séparation de la séquence d'ARN en codons de trois lettres
-  for (let i = 0; i < ARN.length; i += 3) {
-    const codon = ARN.slice(i, i + 3);
-    // Vérification que le codon existe dans le tableau des codons
-    if (codons.hasOwnProperty(codon)) {
-      // Ajout de l'acide aminé correspondant à la séquence de protéine
-      protein += codons[codon] + ' ';
-    } 
-  }
-  
-  return protein.trim();
-}
-
-// Et hop dans le terminal avec un
-const ARN = prompt("Rentre ton ARN à traduire");
-const protein = translateARNtoProtein(ARN);
-console.log(protein); 
-
-
-
-
-
-
-
-
